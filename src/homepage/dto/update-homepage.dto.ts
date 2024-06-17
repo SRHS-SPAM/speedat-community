4503db27fddcb9c99e4ef9c9732b50644f3fd117
haen <142671869+colin2624@users.noreply.github.com>
2024-06-17 19:21:37 +0900

    [restart]

--- src/homepage/dto/update-homepage.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateHomepageDto } from './create-homepage.dto';
-
-export class UpdateHomepageDto extends PartialType(CreateHomepageDto) {}
